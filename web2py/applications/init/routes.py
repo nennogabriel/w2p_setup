ad90c3ddb9df29c83a6bd8f76ef15e5c74ba600e
nennogabriel <nennogabriel@gmail.com>
2016-08-22 21:09:06 -0300

    add routes

---
+++ web2py/applications/init/routes.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from fileutils import abspath
+from languages import read_possible_languages
+
+
+possible_languages = read_possible_languages(abspath('applications', app, 'languages'))
+routers = {
+    app: dict(
+        default_language = possible_languages['default'][0],
+        languages = [lang for lang in possible_languages if lang != 'default'],
+    )
+}
---
+++ web2py/routes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+routers = dict(
+    # base router
+    BASE=dict(
+        default_application='init',
+        # domains = {},
+    ),
+)
+
+
+#
+# routes_onerror = [
+#     (r'*/*', '/_error'),
+#     (r'*/403', r'/static/403.html'),
+#     (r'*/404', r'/static/404.html'),
+#     (r'*/500', r'/static/500.html')
+# ]
+#
