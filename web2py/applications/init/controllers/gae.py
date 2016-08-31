4feaef7fc3b82039a6f88b02f933a47ba79cff96
nennogabriel <nennogabriel@gmail.com>
2016-08-31 16:24:14 -0300

    easy deploy

--- web2py/applications/init/controllers/gae.py
+++ web2py/applications/init/controllers/gae.py
@@ -17,6 +17,7 @@ except:
 
 GAE_APPCFG = os.path.abspath(os.path.join('/usr/local/bin/appcfg.py'))
 
+
 from gluon.settings import settings
 if not settings.is_source:
     session.flash = 'Requires running web2py from source'
@@ -44,9 +45,18 @@ class EXISTS(object):
 
 @auth.requires(lambda: auth.has_membership('app_admin'))
 def index():
-    regex = re.compile('^\w+$')
-    apps = sorted(
-        file for file in os.listdir(apath(r=request)) if regex.match(file))
+    yaml = apath('../app.yaml', r=request)
+    if not os.path.exists(yaml):
+        example = apath('../app.example.yaml', r=request)
+        shutil.copyfile(example, yaml)
+    gae_app_name = gae_app_version = '1'
+    if not request.vars:
+        data = read_file(yaml)
+        comp = re.compile('application:(.*)')
+        gae_app_name = re.findall(comp,data)[0].strip()
+        comp = re.compile('version:(.*)')
+        gae_app_version = re.findall(comp,data)[0].strip()
+
     form = SQLFORM.factory(
         Field('appcfg',
               default=GAE_APPCFG,
@@ -54,14 +64,20 @@ def index():
               requires=EXISTS(error_message=T('file not found'))
               ),
         Field('google_application_id',
-
+              default=gae_app_name,
               requires=IS_MATCH('[\w\-]+'),
               label=T('Google Application Id')
               ),
-        Field('applications', 'list:string',
-              requires=IS_IN_SET(apps, multiple=True),
-              label=T('web2py apps to deploy')),
-        Field('email', requires=IS_EMAIL(), label=T('GAE Email')),
+        Field('version',
+              default=gae_app_version,
+              requires=IS_NOT_EMPTY(),
+              label=T('Version')
+              ),
+        Field('email',
+              default=appconfig.get('google.admin', ''),
+              requires=IS_EMAIL(),
+              label=T('GAE Email')
+              ),
         )
     cmd = output = errors = ""
     if form.accepts(request, session):
@@ -69,20 +85,9 @@ def index():
             kill()
         except:
             pass
-        ignore_apps = [item for item in apps
-                       if not item in form.vars.applications]
-        regex = re.compile('\(applications/\(.*')
-
-        yaml = apath('../app.yaml', r=request)
-        if not os.path.exists(yaml):
-            example = apath('../app.example.yaml', r=request)
-            shutil.copyfile(example, yaml)
-
         data = read_file(yaml)
-        data = re.sub('application:.*', 'application: %s' %
-                      form.vars.google_application_id, data)
-        data = regex.sub(
-            '(applications/(%s)/.*)|' % '|'.join(ignore_apps), data)
+        data = re.sub('application:.*', 'application: %s' % form.vars.google_application_id, data, 1)
+        data = re.sub('version:.*', 'version: %s' % form.vars.version, data, 1)
         write_file(yaml, data)
 
         path = request.env.applications_parent
@@ -93,7 +98,6 @@ def index():
                                                           stdin=s.PIPE,
                                                           stdout=s.PIPE,
                                                           stderr=s.PIPE, close_fds=True), -1)
-        # p.stdin.write(form.vars.password + '\n')
         fcntl.fcntl(p.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
         fcntl.fcntl(p.stderr.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
     return dict(form=form, command=cmd)
