e1979d66cbc6da58d71094a7a22d1568adea2d78
nennogabriel <nennogabriel@gmail.com>
2016-08-31 13:50:58 -0300

    comment yaml

--- web2py/routes.py
+++ web2py/routes.py
@@ -9,11 +9,11 @@ routers = dict(
 
 
 
-routes_onerror = [
-    # (r'*/*', '/init/plugin_error_handler'),
-    (r'*/403', r'/init/static/403.html'),
-    (r'*/404', r'/init/static/404.html'),
-    (r'*/500', r'/init/static/500.html')
-]
-
+# routes_onerror = [
+#     # (r'*/*', '/init/plugin_error_handler'),
+#     (r'*/403', r'/init/static/403.html'),
+#     (r'*/404', r'/init/static/404.html'),
+#     (r'*/500', r'/init/static/500.html')
+# ]
+#
 
